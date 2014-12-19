e55620f91ef89c1df2b898148548a04cbb41fc0f
Dirley Rodrigues <dirleyrls@gmail.com>
2014-12-19 17:24:16 -0200

    include time since tweet

--- src/components/tweetcarousel.jsx
+++ src/components/tweetcarousel.jsx
@@ -1,17 +1,60 @@
-/**
- * TODO Display *time since*
- * TODO Remove dangerouslySetInnerHTML usage.
- */
-
 var React = require('react');
 
 var gsap = require('gsap');
 var gsap_plugin_react = require('gsap-react-plugin');
 
+var Time = require('react-time');
+
+function isNumber(obj) {
+  return toString.call(value) === '[object Number]';
+}
+
+var TimeSince = React.createClass({
+  displayName: 'TimeSince',
+
+  getDefaultProps: function() {
+    return {
+      updateInterval: 5000
+    };
+  },
+
+  componentDidMount: function() {
+    this.resetTicker();
+  },
+
+  componentWillUnmount: function() {
+    this.clearTicker();
+  },
+
+  componentWillReceiveProps: function(nextProps) {
+    if (typeof nextProps.updateInterval !== 'undefined') {
+      this.resetTicker();
+    }
+  },
+
+  clearTicker: function() {
+    if (this.ticker) {
+      return clearInterval(this.ticker);
+    }
+  },
+
+  resetTicker: function() {
+    this.clearTicker();
+    return this.ticker = setInterval(this.invalidate, this.props.updateInterval);
+  },
+
+  invalidate: function() {
+    this.forceUpdate();
+  },
+
+  render: function() {
+    return <Time {...this.props} relative={true} />
+  }
+});
+
 var TweetDisplay = React.createClass({
   render: function() {
-    // TODO FIXME We shouldn't be using *dangerouslySetInnerHTML* here!
-    // It's dangerous!
+    // TODO FIXME *dangerouslySetInnerHTML* is probably dangerous, isn't it?
 
     var quotedText = '"' + this.props.text + '"';
 
@@ -23,6 +66,7 @@ var TweetDisplay = React.createClass({
         </h3>
 
         <p className="comment" dangerouslySetInnerHTML={{__html: quotedText}} />
+        <TimeSince value={this.props.publishedAt} updateInterval={1000} />
       </div>
     );
   }
@@ -45,9 +89,17 @@ var TweetCarousel = React.createClass({
     this.clearTicker();
   },
 
-  componentWillReceiveProps: function() {
-    this.setState({currentIndex: 0});
-    this.resetTicker();
+  componentWillReceiveProps: function(nextProps) {
+    var receivedTweets = typeof nextProps.tweets !== 'undefined';
+    var receivedInterval = typeof nextProps.interval !== 'undefined';
+
+    if (receivedTweets) {
+      this.setState({currentIndex: 0});
+    }
+
+    if (receivedTweets || receivedInterval) {
+      this.resetTicker();
+    }
   },
 
   clearTicker: function() {
@@ -67,6 +119,7 @@ var TweetCarousel = React.createClass({
       nextIndex = 0;
     }
 
+    // Duration of the whole "fade-out, fade-in" animation, in seconds.
     var fadeDuration = 1;
 
     TweenLite.to(this, fadeDuration / 2, {
@@ -79,16 +132,11 @@ var TweetCarousel = React.createClass({
   },
 
   render: function() {
-    // FIXME If there is no current, there should be no TweetDisplay,
-    // but I don't know how to make it's rendering optional. I'm sorry.
-    var current = this.props.tweets.length ? this.props.tweets[this.state.currentIndex] : null;
+    var tweets = this.props.tweets;
+    var current = tweets.length === 0 ? null : tweets[this.state.currentIndex];
 
-    function conditionallyRenderTweetDisplay() {
-      if (current) {
-        return (<TweetDisplay {...current} />);
-      } else {
-        return null;
-      }
+    if (current) {
+      current = <TweetDisplay {...current} />;
     }
 
     var style = {opacity: this.state.opacity};
@@ -96,9 +144,7 @@ var TweetCarousel = React.createClass({
     return (
       <div className="TweetCarousel">
         <h1 className="title">{this.props.title}</h1>
-        <div style={style}>
-          {conditionallyRenderTweetDisplay()}
-        </div>
+        <div style={style}>{current}</div>
       </div>
     );
   }
