c2b4740292627c9b89927275bb66fa697f87297d
Dirley Rodrigues <dirleyrls@gmail.com>
2014-12-20 22:39:39 -0200

    minor refactoring

--- src/components/newslist.jsx
+++ src/components/newslist.jsx
@@ -2,8 +2,6 @@ var React = require('react');
 
 var NewsList = React.createClass({
   render: function() {
-    var unordered = this.props.unordered ? true : false;
-
     function renderItem(item) {
       return (
         <li data-foreach-item="items">
@@ -13,18 +11,20 @@ var NewsList = React.createClass({
       );
     }
 
-    function renderList(items, unordered) {
-      if (unordered) {
-        return (<ol>{items.map(renderItem)}</ol>);
-      } else {
-        return (<ul className="list-nostyle">{items.map(renderItem)}</ul>);
-      }
+    var list;
+    var items = this.props.items;
+    var unordered = this.props.unordered ? true : false;
+
+    if (unordered) {
+      list = <ol>{items.map(renderItem)}</ol>;
+    } else {
+      list = <ul className="list-nostyle">{items.map(renderItem)}</ul>;
     }
 
     return (
       <div className="NewsList">
         <h1 className="title">{this.props.title}</h1>
-        {renderList(this.props.items, unordered)}
+        {list}
         <p className="more-info">{this.props.moreinfo}</p>
         <p className="updated-at">{this.props.updatedAtMessage}</p>
       </div>
